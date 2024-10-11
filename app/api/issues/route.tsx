6460d66caa77b8c8d6099ff8b70c39d832003258
Muzammilishtiaq <tomuzammilishtiaq@gmail.com>
2024-10-11 19:04:20 +0500

    setting up radix ui

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -5,7 +5,7 @@ import { z } from 'zod';
 const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
   description: z.string().min(1),
-});
+});  
 
 export async function POST(request: NextRequest) {
   try {

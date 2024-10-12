5e69909552f8bb56b3f723cc8b3e5332c2bd6d89
Muzammilishtiaq <tomuzammilishtiaq@gmail.com>
2024-10-12 12:38:20 +0500

    handle error

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -3,8 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod';
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1,
+    'Title is Required'
+  ).max(255),
+  description: z.string().min(1, 'Description is Required'),
 });  
 
 export async function POST(request: NextRequest) {
@@ -14,7 +16,7 @@ export async function POST(request: NextRequest) {
     
     // If validation fails, return a 400 response with validation errors
     if (!validation.success) {
-      return NextResponse.json(validation.error.errors, { status: 400 });
+      return NextResponse.json(validation.error.format(), { status: 400 });
     }
 
     // Create a new issue in the database
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+import { Button, Callout, TextArea, TextField } from '@radix-ui/themes'
+import React, { useState } from 'react'
 import { useForm, Controller } from 'react-hook-form'
 import dynamic from 'next/dynamic'
 import axios from 'axios';
@@ -18,17 +18,29 @@ interface issuesFormProps {
 const NewIssues = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<issuesFormProps>();
-
+const[error,setError]=useState('')
   return (
-    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data) => {
+    <div className='max-w-xl '>
+    {error&&<Callout.Root color='red' className='mb-5'>
+         <Callout.Text>
+          {error}
+         </Callout.Text>
+    </Callout.Root>}
+    <form className='space-y-3' onSubmit={handleSubmit(async (data) => {
+     try{
       await axios.post('/api/issues', data);
       router.push('/issues');
       console.log(data);
+     }catch(err:any){
+      console.log(err)
+      setError('An unexpected error occurred.')
+     }
     })}>
       <TextField.Root placeholder="Title..." type='text' {...register('title')} />
       <Controller name='description' control={control} render={({ field }) => <SimpleMDE placeholder='Description...' {...field} />} />
       <Button className='btn btn-primary' type='submit'>Submit New Issue</Button>
     </form>
+    </div>
   )
 }
 
